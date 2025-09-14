1eff66d1e6037a2136f9220e6a969e19071f3f26
doublesharp <jsilver@doublesharp.com>
2025-09-14 13:41:21 -0700

    use test fixtures
    refactor tests to use fixtures to make them more efficient as tests are added.

--- test/hardhat/MyOFT.test.ts
+++ test/hardhat/MyOFT.test.ts
@@ -1,7 +1,7 @@
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { expect } from 'chai'
-import { Contract, ContractFactory } from 'ethers'
+import {  ContractFactory } from 'ethers'
 import { deployments, ethers } from 'hardhat'
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
 
 import { Options } from '@layerzerolabs/lz-v2-utilities'
 
@@ -9,28 +9,14 @@ describe('MyOFT Test', function () {
     // Constant representing a mock Endpoint ID for testing purposes
     const eidA = 1
     const eidB = 2
-    // Declaration of variables to be used in the test suite
-    let MyOFT: ContractFactory
-    let EndpointV2Mock: ContractFactory
-    let ownerA: SignerWithAddress
-    let ownerB: SignerWithAddress
-    let endpointOwner: SignerWithAddress
-    let myOFTA: Contract
-    let myOFTB: Contract
-    let mockEndpointV2A: Contract
-    let mockEndpointV2B: Contract
 
-    // Before hook for setup that runs once before all tests in the block
-    before(async function () {
-        // Contract factory for our tested contract
-        //
+    async function deployOFTFixture() {
         // We are using a derived contract that exposes a mint() function for testing purposes
-        MyOFT = await ethers.getContractFactory('MyOFTMock')
+        const MyOFT = await ethers.getContractFactory('MyOFTMock')
 
         // Fetching the first three signers (accounts) from Hardhat's local Ethereum network
         const signers = await ethers.getSigners()
-
-        ;[ownerA, ownerB, endpointOwner] = signers
+        const [ownerA, ownerB, endpointOwner] = signers
 
         // The EndpointV2Mock contract comes from @layerzerolabs/test-devtools-evm-hardhat package
         // and its artifacts are connected as external artifacts to this project
@@ -40,18 +26,15 @@ describe('MyOFT Test', function () {
         //
         // See https://github.com/NomicFoundation/hardhat/issues/1040
         const EndpointV2MockArtifact = await deployments.getArtifact('EndpointV2Mock')
-        EndpointV2Mock = new ContractFactory(EndpointV2MockArtifact.abi, EndpointV2MockArtifact.bytecode, endpointOwner)
-    })
+        const EndpointV2Mock = new ContractFactory(EndpointV2MockArtifact.abi, EndpointV2MockArtifact.bytecode, endpointOwner)
 
-    // beforeEach hook for setup that runs before each test in the block
-    beforeEach(async function () {
         // Deploying a mock LZEndpoint with the given Endpoint ID
-        mockEndpointV2A = await EndpointV2Mock.deploy(eidA)
-        mockEndpointV2B = await EndpointV2Mock.deploy(eidB)
+        const mockEndpointV2A = await EndpointV2Mock.deploy(eidA)
+        const mockEndpointV2B = await EndpointV2Mock.deploy(eidB)
 
         // Deploying two instances of MyOFT contract with different identifiers and linking them to the mock LZEndpoint
-        myOFTA = await MyOFT.deploy('aOFT', 'aOFT', mockEndpointV2A.address, ownerA.address)
-        myOFTB = await MyOFT.deploy('bOFT', 'bOFT', mockEndpointV2B.address, ownerB.address)
+        const myOFTA = await MyOFT.deploy('aOFT', 'aOFT', mockEndpointV2A.address, ownerA.address)
+        const myOFTB = await MyOFT.deploy('bOFT', 'bOFT', mockEndpointV2B.address, ownerB.address)
 
         // Setting destination endpoints in the LZEndpoint mock for each MyOFT instance
         await mockEndpointV2A.setDestLzEndpoint(myOFTB.address, mockEndpointV2B.address)
@@ -60,11 +43,15 @@ describe('MyOFT Test', function () {
         // Setting each MyOFT instance as a peer of the other in the mock LZEndpoint
         await myOFTA.connect(ownerA).setPeer(eidB, ethers.utils.zeroPad(myOFTB.address, 32))
         await myOFTB.connect(ownerB).setPeer(eidA, ethers.utils.zeroPad(myOFTA.address, 32))
-    })
+
+        return { ownerA, ownerB, myOFTA, myOFTB }
+    }
 
     // A test case to verify token transfer functionality
     it('should send a token from A address to B address via each OFT', async function () {
         // Minting an initial amount of tokens to ownerA's address in the myOFTA contract
+        const { ownerA, ownerB, myOFTA, myOFTB } = await loadFixture(deployOFTFixture)
+
         const initialAmount = ethers.utils.parseEther('100')
         await myOFTA.mint(ownerA.address, initialAmount)
 
